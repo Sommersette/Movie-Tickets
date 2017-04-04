604d4af71449e07fc0aeb9b6ff20ba435084cc4c
James Higgins and Trevor Fast <james@>
2017-04-04 13:35:45 -0700

    fix js to alert correct input

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,32 @@
 // backend logic //
 
-function Ticket(age, time, movie){
+function Ticket(age, time, movie) {
   this.age = age;
   this.time = time;
   this.movie = movie;
-  // this.reciept=[]; might need this, might not...
+  // this.receipt=[]; might need this, might not...
 }
 
 
 
-Ticket.prototype.price = function(){
+Ticket.prototype.price = function() {
   return this.age + " " + this.time + " " + this.movie;
 }
 
 
 // front end //
+
+$(document).ready(function() {
+  $("#form").submit(function(event) {
+    event.preventDefault();
+    var inputtedAge = $("#age").val();
+    var inputtedTime = $("#time").val();
+    var inputtedMovie = $("input:radio[name=movie]:checked").val();
+    alert(inputtedMovie);
+    var newTicket = new Ticket(inputtedAge, inputtedTime, inputtedMovie);
+    // $("ul#receipt").append("<li>" + Ticket.price() + "</li>");
+
+  });
+
+
+});

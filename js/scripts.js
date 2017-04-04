6e6db8ff281d07a445658d10661c57afc8e2d688
James Higgins and Trevor Fast <james@>
2017-04-04 15:33:30 -0700

    fix javascript branching bychanging this statements and correcting return and call. Html fixed image location in div.

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,34 @@ function Ticket(age, time, movie) {
   this.age = age;
   this.time = time;
   this.movie = movie;
+  this.price = 5;
   // this.receipt=[]; might need this, might not...
 }
 
-Ticket.prototype.price = function() {
-  return this.age + " " + this.time + " " + this.movie;
+Ticket.prototype.alterPrice = function() {
+  if ((this.age < 12) || (this.age >= 65)) {
+    this.price += 0
+  }
+  else {
+    this.price += 2
+  }
+  if (this.time >= 17) {
+    this.price += 3
+  }
+  else {
+    this.price += 1
+  }
+  if (this.movie === "oldy"){
+    this.price += 3
+  }
+  else {
+    this.price += 5
+  }
+  return this.price;
 }
+
+
+
 
 // front end //
 
@@ -16,11 +38,10 @@ $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
     var inputtedAge = $("#age").val();
-    var inputtedTime = $("#time").val();
+    var inputtedTime = parseFloat($("#time").val());
     var inputtedMovie = $("input:radio[name=movie]:checked").val();
-    var newTicket = new Ticket("Your age is", inputtedAge, inputtedTime, inputtedMovie);
-    $("#receipt").append("<li>" + newTicket.price() + "</li>");
+    var newTicket = new Ticket(inputtedAge, inputtedTime, inputtedMovie);
+    $("#receipt").append("<li>" + newTicket.alterPrice() + "</li>");
+    newTicket.alterPrice();
   });
-
-
 });
